99e95e635052b9c147531309c371af7525a3a7e2
Evandro Neto <evandro.rsneto@gmail.com>
2026-03-15 19:21:34 -0300

    Add API documentation pages and search functionality
    - Created `search.html` for the API search interface.
    - Added `serialized-form.html` to display serialized forms of classes.
    - Implemented `tag-search-index.js` for search indexing of serialized forms.
    - Introduced `type-search-index.js` for indexing all classes and interfaces in the API.

---
+++ src/main/java/iterum/Iterum.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package iterum;
+
+import iterum.view.MainFrame;
+
+/**
+ *
+ * @author evandro
+ */
+public class Iterum {
+
+    public static void main(String[] args) {
+        MainFrame.main(args);
+    }
+}
